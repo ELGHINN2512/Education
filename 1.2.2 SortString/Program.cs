fbf1a96f244cefcd10a9e93ee1fc21aa1d4f4a32
ELGHINN2512 <65303418+ELGHINN2512@users.noreply.github.com>
2020-10-24 12:22:29 +0300

    Add files via upload

---
+++ 1.2.2 SortString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1._2._2_SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Microsoft .NET Framework 2 Application Development Foundation";
+
+            string[] sa = s.Split(" ");
+            for (int i = 0; i < sa.Length; i++)
+                Console.Write("{0} ",sa[i]);
+            Console.WriteLine();
+
+            Array.Sort(sa);
+            for (int i = 0; i < sa.Length; i++)
+                Console.Write("{0} ", sa[i]);
+            Console.WriteLine();
+
+            s = string.Join(" ", sa); 
+            Console.WriteLine(s);
+        }
+    }
+}
